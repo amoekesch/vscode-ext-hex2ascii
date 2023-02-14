9a5bb82e4e81a71f6dd160e04d386f78afd9fa46
alborrajo <38296782+alborrajo@users.noreply.github.com>
2023-02-14 19:43:25 +0100

    Support for comma separated hex (#1)
    * Support for comma separated hex
    
    * Update vscode engine

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -4,15 +4,31 @@ import { before } from 'mocha';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../extension';
+import * as myExtension from '../extension';
 
 suite('Extension Test Suite', () => {
+
+    const ascii = 'test text';
+    const hex = '74 65 73 74 20 74 65 78 74';
+    const hex0x = '0x74 0x65 0x73 0x74 0x20 0x74 0x65 0x78 0x74';
+    const hexCommaSeparated = '74, 65, 73, 74, 20, 74, 65, 78, 74';
+    const hex0xCommaSeparated = '0x74, 0x65, 0x73, 0x74, 0x20, 0x74, 0x65, 0x78, 0x74';
+
     before(() => {
         vscode.window.showInformationMessage('Start all tests.');
     });
 
-    test('Sample test', () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+    test('hex to ascii', () => {
+        assert.strictEqual(myExtension.hexToAscii(hex), ascii);
+        assert.strictEqual(myExtension.hexToAscii(hex0x), ascii);
+        assert.strictEqual(myExtension.hexToAscii(hexCommaSeparated), ascii);
+        assert.strictEqual(myExtension.hexToAscii(hex0xCommaSeparated), ascii);
     });
+
+    test('ascii to hex', () => {
+        assert.strictEqual(myExtension.asciiToHex(ascii, myExtension.A2H_STD, false), hex);
+        assert.strictEqual(myExtension.asciiToHex(ascii, myExtension.A2H_0X0, false), hex0x);
+        assert.strictEqual(myExtension.asciiToHex(ascii, myExtension.A2H_STD, true), hexCommaSeparated);
+        assert.strictEqual(myExtension.asciiToHex(ascii, myExtension.A2H_0X0, true), hex0xCommaSeparated);
+    })
 });
